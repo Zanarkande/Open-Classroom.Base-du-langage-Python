dfe6988196780caec3c59fb4bd71cf1a03d98f49
Zanarkande <188413966+Zanarkande@users.noreply.github.com>
2025-05-16 23:06:43 -0400

    Update 3-2-Main.py

--- 3-2-Main.py
+++ 3-2-Main.py
@@ -1,21 +1,31 @@
 # Écrivez votre code ici !
 
-import os
+# 3.2 Extrayez et transformez des données avec l’extraction web #
+
 from bs4 import BeautifulSoup
-
-# Obtenir le chemin du script
-base_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(base_dir, "3-2-Index.html")
-
-with open(file_path, 'r') as file:
-    soup = BeautifulSoup(file, 'html.parser')
+with open("/Users/Allan/Document/Code/Python/Open-Classroom.Base-du-langage-Python/3-2-Index.html", "r") as file:
+    soup = BeautifulSoup(file.read(), 'html.parser')
 
 title = soup.title.string
+
 h1 = soup.find('h1').string
 
 all_products = dict()
+
 products = soup.find_all("li")
 
-#print(products)
+#Extraction et transformation du nom, du prix et de la description de chaque produit puis load dans un dictionnaire
 for product in products:
-    print(product.find("h2").string)
+    name_products = product.find("h2").string
+    price_products = product.find("p", class_="price").string
+    price_list = price_products.split(":")
+    all_products[name_products] = {"prix": float(price_list[1].replace('€', '').replace(' ', ''))}
+    description_product = product.find_all('p')[-1].string
+    description_list = description_product.split(':')
+    description_clean = description_list[1][1:]
+    all_products[name_products]['description'] = description_clean
+
+# Conversion du prix en dollar
+for product in (all_products):
+    prix = all_products[product]['prix']*1.2
+    all_products[product]['prix'] = prix
